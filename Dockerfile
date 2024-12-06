7d8235da7501f5d52b401a9aed59b7cdf1eac12d
aroglahcim <aroglahcim@gmail.com>
2024-12-06 19:38:35 +0100

    feat: add upsd reload

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM ruby:3.3.5-slim-bookworm
 
 RUN apt-get update && \
-    apt-get install -y nut nut-client nut-server
+    apt-get install -y nut nut-client nut-server curl
 
 RUN useradd -s /bin/bash exporter
 RUN chown -R exporter:exporter /etc/nut /run/nut
--- metrics.rb
+++ metrics.rb
@@ -1,8 +1,13 @@
 require 'socket'
-server = TCPServer.new 80
 
 system("upsdrvctl start")
-system("upsd")
+upsd_result = system("upsd")
+
+if !upsd_result
+    system("upsd -c reload")
+end
+
+server = TCPServer.new 80
 
 PREFIX = "upsc_"
 HOSTNAME = "localhost"
@@ -18,10 +23,11 @@ def print_metrics_for_ups(session, ups_name)
     metrics_raw = %x|upsc #{ups_name}@#{HOSTNAME} #{STDERR_REDIRECT}|
     metrics = metrics_raw.split("\n")
 
-    for index in 0 ... metrics.size
-        metric = metrics[index].split(":", 2)
+    metrics.each do |row|
+        metric = row.split(":", 2)
         name="#{PREFIX}#{metric[0].gsub(".","_")}"
         value = metric[1]
+
         if is_number?(value)
             session.print "\# TYPE #{name} gauge\n"
             session.print "#{name}{ups=\"#{ups_name}\"} #{value}\n"
@@ -34,16 +40,14 @@ while session = server.accept
     request = session.gets
     puts request 
 
-    upses_raw = %x|upsc -L #{STDERR_REDIRECT}|
+    upses_raw = %x|upsc -l #{STDERR_REDIRECT}|
     upses = upses_raw.split("\n")
     
     session.print "HTTP/1.1 200\n"
     session.print "Content-Type: text/plain\n"
     session.print "\n"
 
-    upses.each do |ups|
-        parts = ups.split(":", 2)
-        ups_name = parts[0]
+    upses.each do |ups_name|
         print_metrics_for_ups(session, ups_name)
     end
 
